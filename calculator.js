e8c6b3b24185594e24ed19929931fc2c134f89af
Ana Segarra <anasegarraber@gmail.com>
2020-05-17 11:16:21 +0200

    Implement basic operations with two factors

--- calculator.js
+++ calculator.js
@@ -1,8 +1,10 @@
 class Calculator {
   constructor() {
-    this.firstFactor = 0;
-    this.secondFactor;
+    this.firstFactor = '0';
+    this.secondFactor = '0';
+    this.operation;
   }
+
   add(num1, num2) {
     return Number(num1) + Number(num2);
   }
@@ -14,5 +16,25 @@ class Calculator {
   }
   divide(num1, num2) {
     return Number(num1) / Number(num2);
+  }
+  handleOperation() {
+    let result;
+    switch (this.operation) {
+      case '+':
+        result = this.add(this.firstFactor, this.secondFactor);
+        break;
+      case '-':
+        result = this.subtract(this.firstFactor, this.secondFactor);
+        break;
+      case 'x':
+        result = this.multiply(this.firstFactor, this.secondFactor);
+        break;
+      case '÷':
+        result = this.divide(this.firstFactor, this.secondFactor);
+        break;
+      default:
+        result = document.getElementById('display').textContent;
+    }
+    return result;
   }
 }
--- main.js
+++ main.js
@@ -8,24 +8,55 @@ window.addEventListener('load', () => {
   document.querySelectorAll('.number').forEach(num =>
     num.addEventListener('click', e => {
       console.log(e.target.textContent);
-      display.textContent =
-        calculator.firstFactor === 0
-          ? e.target.textContent
-          : display.textContent + e.target.textContent;
-      calculator.firstFactor = display.textContent;
+      if (!calculator.operation) {
+        console.log('first factor');
+        display.textContent =
+          calculator.firstFactor === '0'
+            ? e.target.textContent
+            : display.textContent + e.target.textContent;
+        calculator.firstFactor = display.textContent;
+      } else {
+        console.log('second factor');
+        display.textContent =
+          calculator.secondFactor === '0'
+            ? e.target.textContent
+            : display.textContent + e.target.textContent;
+        calculator.secondFactor = display.textContent;
+      }
     })
   );
 
   decimal.addEventListener('click', () => {
-    console.log('adding decimal dot');
     if (!display.textContent.includes('.')) {
+      console.log('adding decimal dot');
       display.textContent += '.';
+      calculator.operation
+        ? (calculator.secondFactor = display.textContent)
+        : (calculator.firstFactor = display.textContent);
     }
   });
 
   clear.addEventListener('click', () => {
     console.log('clearing screen');
-    display.textContent = 0;
-    calculator.firstFactor = 0;
+    display.textContent = '0';
+    calculator.firstFactor = '0';
+    calculator.secondFactor = '0';
+    calculator.operation = null;
+  });
+
+  document.querySelectorAll('.operations').forEach(operation => {
+    operation.addEventListener('click', e => {
+      console.log(e.target.textContent);
+      calculator.operation = e.target.textContent;
+    });
+  });
+
+  document.getElementById('equal').addEventListener('click', () => {
+    const result = calculator.handleOperation();
+    console.log('resultado', result);
+    display.textContent = result;
+    calculator.firstFactor = result;
+    calculator.secondFactor = '0';
+    calculator.operation = null;
   });
 });
